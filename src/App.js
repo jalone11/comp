c38f7d65a6fa6142069dee3dc9f0b153c0ef27de
jalone11 <abdeljelil.behi@yahoo.com>
2020-08-23 18:30:21 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -2,25 +2,18 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import React from 'react';
+import { Container } from 'reactstrap';
+
+const Example = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Container className="Product">.container</Container>
+      <Container className="Name" fluid="sm">.container-sm</Container>
+      <Container className="Category" fluid="md">.container-md</Container>
+      <Container className="price" fluid="lg">.container-lg</Container>
+      
   );
 }
 
-export default App;
+export default Example;
